5554f8fa8b28c38cdce030658ec2c2896e9642cd
art81 <42809343+art81@users.noreply.github.com>
2021-08-27 12:34:00 -0400

    Implement hand detection and tracking. other minor changes

--- tello_project.py
+++ tello_project.py
@@ -1,6 +1,8 @@
 import cv2
 import time
 import numpy as np
+import mediapipe as mp
+from google.protobuf.json_format import MessageToDict
 
 from djitellopy import Tello as djiTello
 
@@ -8,11 +10,12 @@ from djitellopy import Tello as djiTello
 WIDTH  = -1 # DEFAULT 960
 HEIGHT = -1 # DEFAULT 720
 DONT_FLY = True
+DISPLAY  = True
 
 # Class to run YOLO Object Detection and Localization and is based on https://github.com/arunponnusamy/object-detection-opencv
 class MachineVision():
     # Constructor
-    def __init__(self):
+    def __init__(self, display):
         yoloPrePath = "models/Yolo/"
         self.yolo_filenames = {"class"  : yoloPrePath + "yolov3.txt",
                                "weight" : yoloPrePath + "yolov3.weights",
@@ -31,9 +34,19 @@ class MachineVision():
         self.yolo_net = cv2.dnn.readNet(self.yolo_filenames["weight"], self.yolo_filenames["config"])
         self.fd_net   = cv2.dnn.readNetFromCaffe(self.fd_filenames["config"], self.fd_filenames["model"])
 
+        # Import hand detection and tracking
+        MAX_NUM_HANDS = 2
+        MIN_DETECTION_CONFIDENCE = 0.5
+        MIN_TRACKING_CONFIDENCE  = 0.5
+        self.mpHands = mp.solutions.hands
+        self.hands   = self.mpHands.Hands(False, MAX_NUM_HANDS, MIN_DETECTION_CONFIDENCE, MIN_TRACKING_CONFIDENCE)
+        self.mpDraw  = mp.solutions.drawing_utils
+
         # Class indexes for specific objects
         self.yolo_class_indexes = {"person" : self.classes.index("person"),
                                    "dog"    : self.classes.index("dog")}
+
+        self.DISPLAY = display
 
     # Internal function for getting list of output_layers that need to be read from.
     # This is needed because YOLO architecture has multiple output layers
@@ -50,7 +63,7 @@ class MachineVision():
         cv2.putText(img, label + "(" + str(round(confidence, 3)) + ")", (x-10,y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
 
     # Function to return predictions on an inputted image
-    def yolo_predict(self, image, display = False, confidence_thresh = 0.5):
+    def yolo_predict(self, image, confidence_thresh = 0.5):
         Height, Width = image.shape[:2]
 
         # Run image through pre-trained network
@@ -91,16 +104,70 @@ class MachineVision():
             nmsBoxes.append(box)
             nmsConf.append(confidences[i])
             nmsClassId.append(class_ids[i])
-            if display:
+            if self.DISPLAY:
                 self.draw_prediction(image, class_ids[i], confidences[i], round(x), round(y), round(x+w), round(y+h))
 
-        if display:
+        if self.DISPLAY:
             cv2.imshow("object detection", image)
             cv2.waitKey(1)
 
         return nmsBoxes, nmsConf, nmsClassId
 
-    def facial_detection(self, image, display):
+    def hand_detection_and_tracking(self, image):
+        imgRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        h, w, c = image.shape
+        self.results = self.hands.process(imgRGB)
+
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
+                if self.DISPLAY:
+                    self.mpDraw.draw_landmarks(image, handLms, self.mpHands.HAND_CONNECTIONS)
+
+        # Pack landmark info into a list of of dictionaries defining locations of specific hand landmarks
+        handNo = 0
+        landmarkList = []
+        if self.results.multi_hand_landmarks:
+            numHands = len(self.results.multi_hand_landmarks)
+            landmarkList = np.zeros((numHands, 21, 2))
+            for handIdx, currHand in enumerate(self.results.multi_hand_landmarks):
+                # Process landmarks for each hand
+                for id, lm in enumerate(currHand.landmark):
+                    cx, cy = int(lm.x * w), int(lm.y * h) # output is given in percentage [0, 1] - convert to pixels in our image
+
+                    landmarkList[handIdx][id] = [cx, cy]
+
+        # Parse handedness result and print for debug (if display is on)
+        handednessList = []
+        if self.results.multi_handedness:
+            handednessList = [0] * numHands
+            for idx, currHandedness in enumerate(self.results.multi_handedness):
+                dict = MessageToDict(currHandedness)
+                dict = dict['classification'][0]
+
+                #index = dict['index']
+                score = dict['score']
+                label = "Right" if dict['label'] == "Left" else "Left" # flip handedness because we didn't flip input image
+
+                handednessList[idx] = [score, label]
+
+        if self.DISPLAY:
+            # Add labelling of enum points to the output
+            for idx, hand in enumerate(landmarkList):
+                for lm_id, lm in enumerate(hand):
+                    tag = str(self.mpHands.HandLandmark(lm_id))
+                    landmarkName = tag.split(".")[-1]
+
+
+                    color = (0, 0, 255) if handednessList[idx][1] == "Right" else (0, 255, 0)
+                    cv2.putText(image, landmarkName, (int(lm[0]), int(lm[1]-10)), cv2.FONT_HERSHEY_SIMPLEX, 0.3, color, 1)
+                    if landmarkName == "WRIST":
+                        cv2.putText(image, handednessList[idx][1], (int(lm[0]), int(lm[1]+10)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1)
+
+            cv2.imshow("HAND detection", image)
+            cv2.waitKey(1)
+        return image, landmarkList, handednessList
+
+    def facial_detection(self, image):
         h, w = image.shape[:2]
 
         # Run image through pre-trained network
@@ -117,12 +184,12 @@ class MachineVision():
                 [x, y, x1, y1] = faces[0, 0, i, 3:7]*np.array([w, h, w, h]) # first item represents top left and bottom right corner ass x, y, x2, y2 in [0, 1]. Then multiply by h and width to get approx number of pixels from [0, 1] output
                 box = [round(x), round(y), round(x1), round(y1)] # round to get bounding box in pixels
 
-                if display:
+                if self.DISPLAY:
                     color = (0, 0, 255)
                     cv2.rectangle(image, (box[0], box[1]), (box[2], box[3]), color, 2)
                     cv2.putText(image, "face (" + str(round(confidence, 3)) + ")", (box[0]-10,box[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
 
-        if display:
+        if self.DISPLAY:
             cv2.imshow("facial recognition", image)
             cv2.waitKey(1)
 
@@ -180,7 +247,7 @@ def main():
     # Connect to tello
     firstTick = True
     tello = Tello(WIDTH, HEIGHT, DONT_FLY)
-    mv = MachineVision()
+    mv = MachineVision(DISPLAY)
 
     # Main control loop
     while True:
@@ -195,8 +262,12 @@ def main():
         img = tello.getImage()
 
         # Do Object Detection and Localization on the image
-        boxes, confidences = mv.facial_detection(img, display = True)
-        boxes, confidences, class_ids = mv.yolo_predict(img, display = True)
+        boxes, confidences = mv.facial_detection(img.copy())
+        boxes, confidences, class_ids = mv.yolo_predict(img.copy())
+
+        # Do hand detection
+        foundHands_img, landmarkList, handednessList = mv.hand_detection_and_tracking(img.copy())
+
 
         #cv2.imshow("MyResult", img)
 
